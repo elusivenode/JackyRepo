ef4f1ec4ac01042658d904d9f347c2d5bf5ad792
Hamish MacDonald <elusivenode@gmail.com>
2023-11-18 16:19:04 +1000

    Revert "start of number adder code"
    This reverts commit 4c1ca9408ac8d30d69b401628d264da63e942505.

--- number_adder/number_adder.py
+++ number_adder/number_adder.py
@@ -1,8 +1,3 @@
-def add_to_int(i):
-    n = list(range(1,i+1))
-    s = sum(n)
-    return s
-
 with open('/Users/hamish.macdonald/Dev/JackyRepo/number_adder/sample_numbers.txt', 'r') as input_f:
     input_lines = input_f.readlines()
 
@@ -10,5 +5,4 @@ input_numbers = []
 for l in input_lines[:]:
     input_numbers.append(int(l.strip()))
 
-for n in input_numbers:
-    print(f'Sum from 1 to {n} is {add_to_int(n)}.')
+print(input_numbers)
