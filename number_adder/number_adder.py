4c1ca9408ac8d30d69b401628d264da63e942505
Hamish MacDonald <elusivenode@gmail.com>
2023-11-18 16:16:32 +1000

    start of number adder code

--- number_adder/number_adder.py
+++ number_adder/number_adder.py
@@ -1,3 +1,8 @@
+def add_to_int(i):
+    n = list(range(1,i+1))
+    s = sum(n)
+    return s
+
 with open('/Users/hamish.macdonald/Dev/JackyRepo/number_adder/sample_numbers.txt', 'r') as input_f:
     input_lines = input_f.readlines()
 
@@ -5,4 +10,5 @@ input_numbers = []
 for l in input_lines[:]:
     input_numbers.append(int(l.strip()))
 
-print(input_numbers)
+for n in input_numbers:
+    print(f'Sum from 1 to {n} is {add_to_int(n)}.')
