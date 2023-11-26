b9745b41d4a1e23a98e2a0afb8d6d7711574b991
Hamish MacDonald <elusivenode@gmail.com>
2023-11-26 18:38:43 +1000

    questionnaire.py

---
+++ questionnaire/questionnaire.py
@@ -0,0 +1,4 @@
+print('This is Jack\'s Questionnaire program!')
+user_name = input('What is your name? ')
+user_age = input('How old are you? ')
+print(f'Hello {user_name}. Nice to meet you. You are {user_age} years old.')
